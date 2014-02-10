c1d1e8fc89657a24c6854f97a533773a0d279d61
John <JohnRobertWade@gmail.com>
2014-02-09 17:01:42 -0800

    Commit update 3
    Corrected turkey.rb and thanksgiving_dinner.rb and completed first two
    tests for mid_term_spec.rb

--- midterm/thanksgiving_dinner.rb
+++ midterm/thanksgiving_dinner.rb
@@ -1,7 +1,7 @@
-class Turkey
+class ThanksgivingDinner
 	
 	def initilize()
-		
+		@t_dinner.guests = []
 	end
 
 end
--- midterm/turkey.rb
+++ midterm/turkey.rb
@@ -1,7 +1,18 @@
-class ThanksgivingDinner
-
-	def initilize()
-		@t_dinner.guests = []
-	end
+class Animal
 
 end
+
+class Turkey < Animal
+	attr_accessor :weight
+
+	def initialize(w)
+		@weight = w
+	end
+
+	def weight
+		@weight
+	end
+end
+
+# my_turkey = Turkey.new(100)
+# puts my_turkey
