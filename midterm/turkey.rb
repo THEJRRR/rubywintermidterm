137047594560e59e1650af5965627b9bd159e624
John <JohnRobertWade@gmail.com>
2014-02-09 17:43:10 -0800

    Commit 4
    Completed turkey.rb

--- midterm/turkey.rb
+++ midterm/turkey.rb
@@ -12,6 +12,29 @@ class Turkey < Animal
 	def weight
 		@weight
 	end
+
+	def gobble_speak(message)
+		message_array = message.split(" ") 
+		message_array.map! do |x| 
+			@end_in_period = false
+			if x.match(/\./)
+				@end_in_period = true
+			end
+			if x.match(/^[a-z]/)
+				x = "gobble"
+			elsif x.match(/'/)
+				x = "Gobb'le"
+			else
+				x = "Gobble"
+			end
+			if @end_in_period
+				x = x + "."
+			else
+				x = x
+			end	
+		end
+	message_array.join(" ")	
+	end
 end
 
 # my_turkey = Turkey.new(100)
